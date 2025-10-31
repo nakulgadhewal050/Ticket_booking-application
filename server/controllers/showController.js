708145ef6a3ac823af0a6d6e4926e3165b446834
nakul gadhewal <nakulgadhewal050@gmail.com>
2025-10-31 18:34:40 +0530

    add send notification

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import Movie from "../models/Movie.js";
 import Show from "../models/Show.js";
 import { fetchFromTMDB } from "../utils/fetchFromTMDB.js";
+import { inngest } from "../inngest/index.js";
 
 
 //API to get now playing movies from TMDB
@@ -69,6 +70,13 @@ export const addShow = async (req, res) => {
         if (showsToCreate.length > 0) {
             await Show.insertMany(showsToCreate);
         }
+
+        // Trigger Inngest event
+
+        await inngest.send({
+            name: "app/show.added",
+            data: { movieTitle: movie.title }
+        })
 
         res.status(200).json({ success: true, message: "Show added successfully" });
 
